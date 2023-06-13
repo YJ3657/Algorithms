a83d8b71443a91069a3f96ab4e1297ff82fa5ec0
YJ3657 <yeongjay2@gmail.com>
2023-06-13 10:57:12 +0900

    Modified existing files and added fast i/o files

---
+++ Python/Misc/FastIO.py
@@ -0,0 +1,86 @@
+import sys, io, os
+from io import BytesIO, IOBase
+from types import GeneratorType
+
+""" This python file lists ways of getting the input and 
+printing the output in much faster way than simple operation
+such as following a, b = map(int, input().split()).
+"""
+_str = str
+str = lambda x=b"": x if type(x) is bytes else _str(x).encode()
+BUFSIZE = 8192
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+class FastIO:
+    # First Way (Using sys):
+    input = sys.stdin.readline
+    print = sys.stdout.write
+    
+    # Second Way (Using io library):
+    input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+    print = sys.stdout.write(" ".join(map(str,list)) + "\n")
+    
+    # Third Way (Using the I/O class defined by the user - see the class above)
+    sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+    input = lambda: sys.stdin.readline().rstrip("\r\n")
+    output = lambda x: sys.stdout.write(x)
+
+    # Side Track (Use the code below to speed up the recursion using stack)
+    def bootstrap(f, stack=[]):
+        def wrappedfunc(*args, **kwargs):
+            if stack:
+                return f(*args, **kwargs)
+            else:
+                to = f(*args, **kwargs)
+                while True:
+                    if type(to) is GeneratorType:
+                        stack.append(to)
+                        to = next(to)
+                    else:
+                        stack.pop()
+                        if not stack:
+                            break
+                        to = stack[-1].send(to)
+                return to
+        return wrappedfunc
